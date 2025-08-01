8fb20f51cec3366cfb64510b39390cb9a64004c1
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-08-01 15:33:56 -0500

    Update kaggle_pandas.py

--- kaggle/kaggle_pandas.py
+++ kaggle/kaggle_pandas.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+'''
+Creating, Reading, and Writing
+'''
 # Data frames
 fruits = pd.DataFrame({"Apples": [30], "Bananas": [21]})
 fruit_sales = pd.DataFrame({"Apples": [35, 41], "Bananas": [21,34]}, index = ["2017 Sales", "2018 Sales"])
@@ -14,3 +17,13 @@ reviews = pd.read_csv("../input/wine-reviews/winemag-data_first150k.csv", index_
 animals = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=['Year 1', 'Year 2'])
 animals
 animals.to_csv("cows_and_goats.csv")
+
+
+'''
+Indexing, Selecting, and Assigning
+'''
+# Native accesors: let "reviews" be dataframe
+reviews['country'][0]
+reviews.country
+
+# Indexing
