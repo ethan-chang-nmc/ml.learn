9b67b9a81ca563787a9fbe1308121b396339716f
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-08-04 10:57:56 -0500

    Update kaggle_pandas.py

--- kaggle/kaggle_pandas.py
+++ kaggle/kaggle_pandas.py
@@ -31,4 +31,8 @@ reviews.iloc[:, 0] # all rows, column 0
 reviews.iloc[1:3, 0]
 reviews.iloc[[0, 1, 2], 0]
 reviews.iloc[-5:] # last 5 elements
+reviews.loc[0, 'country'] # loc is label based - iloc treats dataset like big matrix (list of lists) to index into postion while loc uses information in indices
+reviews.loc[:, ['taster_name', 'taster_twitter_handle', 'points']] # [0:10] - iloc stdlib indexing (exclusive) but loc inclusive of the 10
 
+# Manipulating Index: index is mutable
+reviews.set_index("title")
