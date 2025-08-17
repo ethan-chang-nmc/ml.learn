235f823523f4741398b9c162d4353f4937cbbf1b
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-08-17 01:57:26 -0500

    Update kaggle_pandas.py

--- kaggle/kaggle_pandas.py
+++ kaggle/kaggle_pandas.py
@@ -174,3 +174,6 @@ point_strings = reviews.points.astype('str')
 
 # Sometimes the price column is null. How many reviews in the dataset are missing a price?
 n_missing_prices = reviews.price.isnull().sum()
+
+# What are the most common wine-producing regions? Create a Series counting the number of times each value occurs in the region_1 field. This field is often missing data, so replace missing values with Unknown. Sort in descending order. Your output should look something like this:
+reviews_per_region = reviews.region_1.fillna("Unknown").value_counts().sort_values(ascending = False)
