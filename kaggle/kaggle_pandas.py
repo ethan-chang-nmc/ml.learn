d43476dcb5170d4582b3bdb00981cd348fcf404b
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-08-17 01:18:40 -0500

    Update kaggle_pandas.py

--- kaggle/kaggle_pandas.py
+++ kaggle/kaggle_pandas.py
@@ -172,3 +172,5 @@ dtype = reviews.points.dtype
 # Create a Series from entries in the points column, but convert the entries to strings. Hint: strings are str in native Python.
 point_strings = reviews.points.astype('str')
 
+# Sometimes the price column is null. How many reviews in the dataset are missing a price?
+n_missing_prices = reviews.price.isnull().sum()
